d754493fa9ef7319da762194c0142e116ab9dbf6
Sunanda123-png <sunanda02@gmail.com>
2021-08-10 16:32:27 +0530

    Adding part time employee

--- src/com/sunanda/Wages.java
+++ src/com/sunanda/Wages.java
@@ -1,23 +1,25 @@
 package com.sunanda;
 
 public class Wages {
-    int IS_FULL_TIME = 1;
+    int PART_TIME = 1;
+    int IS_FULL_TIME = 2;
     int EMP_PER_HOUR_RATE=20;
 
     public void calculation()
     {
         int emp_hour=0;
         int emp_wages=0;
-        double empCheck = Math.floor(Math.random()*10)%2;
+        double empCheck = Math.floor(Math.random()*10)%3;
         if (empCheck==IS_FULL_TIME)
         {
             emp_hour=8;
         }
-
         else
         {
-            emp_hour=0;
+            emp_hour=4;
         }
+
+
         emp_wages=emp_hour*EMP_PER_HOUR_RATE;
         System.out.println("Employee wages"+emp_wages);
     }

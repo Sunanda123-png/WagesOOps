6a21329735eaaa185bbca5ca548c383d6f980e53
Sunanda123-png <sunanda02@gmail.com>
2021-08-10 16:59:23 +0530

    Adding switch case in oops program

--- src/com/sunanda/Wages.java
+++ src/com/sunanda/Wages.java
@@ -1,23 +1,28 @@
 package com.sunanda;
 
 public class Wages {
-    int PART_TIME = 1;
-    int IS_FULL_TIME = 2;
-    int EMP_PER_HOUR_RATE=20;
+    public static final int PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_PER_HOUR_RATE=20;
 
     public void calculation()
     {
         int emp_hour=0;
         int emp_wages=0;
-        double empCheck = Math.floor(Math.random()*10)%3;
-        if (empCheck==IS_FULL_TIME)
+        int empCheck =(int) Math.floor(Math.random()*10)%3;
+        switch(empCheck)
         {
-            emp_hour=8;
+            case IS_FULL_TIME:
+                emp_hour=8;
+                break;
+            case PART_TIME:
+                emp_hour=4;
+                break;
+            default:
+                emp_hour=0;
+                break;
         }
-        else
-        {
-            emp_hour=4;
-        }
+
 
 
         emp_wages=emp_hour*EMP_PER_HOUR_RATE;

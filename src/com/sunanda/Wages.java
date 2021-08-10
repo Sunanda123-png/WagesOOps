06f0cf75a668ca0cad1caf881817cd9cbfcc3d35
Sunanda123-png <sunanda02@gmail.com>
2021-08-10 17:43:56 +0530

    For 20 working days

--- src/com/sunanda/Wages.java
+++ src/com/sunanda/Wages.java
@@ -4,29 +4,34 @@ public class Wages {
     public static final int PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_PER_HOUR_RATE=20;
+    public static final int Total_Working_Days=20;
 
     public void calculation()
     {
         int emp_hour=0;
         int emp_wages=0;
-        int empCheck =(int) Math.floor(Math.random()*10)%3;
-        switch(empCheck)
+        int total_emp_wages=0;
+        int i;
+        for(i=0;i<=20;i++)
         {
-            case IS_FULL_TIME:
-                emp_hour=8;
-                break;
-            case PART_TIME:
-                emp_hour=4;
-                break;
-            default:
-                emp_hour=0;
-                break;
+            int empCheck =(int) Math.floor(Math.random()*10)%3;
+            switch(empCheck)
+            {
+                case IS_FULL_TIME:
+                    emp_hour=8;
+                    break;
+                case PART_TIME:
+                    emp_hour=4;
+                    break;
+                default:
+                    emp_hour=0;
+                    break;
+            }
+            emp_wages=emp_hour*EMP_PER_HOUR_RATE;
+            total_emp_wages +=emp_wages;
+            System.out.println("Employee wages"+emp_wages);
         }
 
-
-
-        emp_wages=emp_hour*EMP_PER_HOUR_RATE;
-        System.out.println("Employee wages"+emp_wages);
     }
 
 }

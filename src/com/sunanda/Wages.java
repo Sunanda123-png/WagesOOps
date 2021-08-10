685ba6458bc95582bf321a22baf4aab95575b583
Sunanda123-png <sunanda02@gmail.com>
2021-08-10 15:43:41 +0530

    Calculate for 8hrs

--- src/com/sunanda/Wages.java
+++ src/com/sunanda/Wages.java
@@ -2,18 +2,26 @@ package com.sunanda;
 
 public class Wages {
     int IS_FULL_TIME = 1;
+    int EMP_PER_HOUR_RATE=20;
+
     public void calculation()
     {
+        int emp_hour=0;
+        int emp_wages=0;
         double empCheck = Math.floor(Math.random()*10)%2;
         if (empCheck==IS_FULL_TIME)
         {
-            System.out.println("Employee is present");
+            emp_hour=8;
         }
+
         else
         {
-            System.out.println("Employee is not present");
+            emp_hour=0;
         }
+        emp_wages=emp_hour*EMP_PER_HOUR_RATE;
+        System.out.println("Employee wages"+emp_wages);
     }
+
 }
 class Empwages
 {
@@ -21,5 +29,6 @@ class Empwages
     {
         Wages eWages=new Wages();
         eWages.calculation();
+
     }
 }
